790dad6f4c71344ea43506a987083193345458e8
Dev Lanageuse <dev@lanageuse.com>
2025-06-11 14:54:44 +0200

    feat: add header component with logo and responsive navigation styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import { NavLink } from "react-router";
+import logo from "../assets/logo.svg"
 
 function Header() {
     return (
         <header>
+            <img src={logo} alt="Kasa" className="logo" />
             <nav>
-                <NavLink to="/">Accueil</NavLink> 
-                <NavLink to="/about">A propos</NavLink> 
+                <NavLink to="/">Accueil</NavLink>
+                <NavLink to="/about">A propos</NavLink>
             </nav>
         </header>
     )
