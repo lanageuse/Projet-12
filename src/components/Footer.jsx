45619bdcedd59372fa15fdd4be63d6cff751d8de
Dev Lanageuse <dev@lanageuse.com>
2025-06-11 16:58:49 +0200

    Make header and footer logos clickable navigation links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,9 @@
+import { NavLink } from "react-router";
 import logo from '../assets/logo-footer.svg'
 function Footer() {
     return (
         <footer>
-            <img src={logo} alt="Kasa" />
+            <NavLink to="/"><img src={logo} alt="Kasa" /></NavLink>
             <div className='footer-container'><p>© 2020 Kasa.</p><p>All rights reserved</p></div>
         </footer>
     )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,12 @@ import logo from "../assets/logo.svg"
 function Header() {
     return (
         <header>
-            <img src={logo} alt="Kasa" className="logo" />
+            <NavLink to="/"><img src={logo} alt="Kasa" className="logo" /></NavLink>
             <nav>
                 <NavLink to="/">Accueil</NavLink>
                 <NavLink to="/about">A propos</NavLink>
             </nav>
-        </header>
+        </header >
     )
 }
 
