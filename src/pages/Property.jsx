4f27d85d27b2e8e6d4243b6c069d4a7d0f1dbb94
Dev Lanageuse <dev@lanageuse.com>
2025-06-12 12:20:29 +0200

    feat: implement complete Property page with dynamic data fetching and component layout

--- src/pages/Property.jsx
+++ src/pages/Property.jsx
@@ -1,10 +1,37 @@
 import { useParams } from "react-router";
-function Property(){
-    let property = useParams();
-    return(
-        <div>
-        {property.id}
-        </div>
+import useListings from '../hook/useListings'
+import { useEffect, useState } from "react"
+import Details from "../components/Property/Details"
+import Host from "../components/Property/Host"
+import Rate from "../components/Property/Rate"
+import Tags from "../components/Property/Tags"
+import Title from "../components/Property/Title"
+import PropertyLayout from "../layouts/PropertyLayout"
+import Hero from "../components/Property/Hero";
+
+function Property() {
+    const [property, setProperty] = useState(null)
+    const { id: paramId } = useParams()
+    const listings = useListings()
+
+    useEffect(() => {
+        if (Array.isArray(listings) && listings.length > 0 && paramId) {
+            const found = listings.find((prop) => prop?.id === paramId)
+            setProperty(found || null)
+        }
+    }, [listings, paramId])
+
+    if (!property) return (<p>Chargement ...</p>)
+
+    return (
+        <PropertyLayout
+            hero={<Hero pictures={property.pictures} />}
+            title={<Title title={property.title} location={property.location} />}
+            tags={<Tags tags={property.tags} />}
+            host={<Host title={property.host} />}
+            rate={<Rate title={property.rate} />}
+            details={<Details description={property.description} equipments={property.equipments} />}
+        />
     )
 }
 
