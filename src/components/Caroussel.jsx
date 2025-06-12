6f683c488d108dd30376a24aa3bfd55baf37dc21
Dev Lanageuse <dev@lanageuse.com>
2025-06-12 12:12:19 +0200

    feat: implement carousel component with navigation and state tracking

--- src/components/Caroussel.jsx
+++ src/components/Caroussel.jsx
@@ -1,5 +1,46 @@
-function Caroussel() {
-    return (<></>)
+import arrowRight from '../assets/arrow-right.svg'
+import arrowLeft from '../assets/arrow-left.svg'
+import { useEffect, useState } from 'react';
+function Caroussel({ children }) {
+    const [currentIndex, setCurrentIndex] = useState(0)
+    const [length, setLength] = useState(children.length)
+    const next = () => {
+        if (currentIndex < (length - 1)) {
+            setCurrentIndex(prev => prev + 1)
+        } else {
+            setCurrentIndex(0)
+        }
+    }
+    const prev = () => {
+        if (currentIndex > 0) {
+            setCurrentIndex(prev => prev - 1)
+        } else {
+            setCurrentIndex(length - 1)
+        }
+    }
+    useEffect(() => {
+        setLength(children.length)
+    }, [children])
+
+    return (
+        <div className="carousel-container">
+            <div className="carousel-wrapper">
+                <button className="left-arrow" onClick={prev}>
+                    <img src={arrowLeft} alt="précédent" />
+                </button>
+
+                <div className="carousel-content-wrapper">
+                    <div className="carousel-content" style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
+                        {children}
+                    </div>
+                </div>
+                <button className="right-arrow" onClick={next}>
+                    <img src={arrowRight} alt="suivant" />
+                </button>
+                <div className='current-state'>{`${currentIndex + 1} / ${length}`}</div>
+            </div>
+        </div>
+    )
 }
 
 export default Caroussel;
