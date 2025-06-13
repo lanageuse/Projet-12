8e6603e9a485c4921aeb2377c3cad2faf3367418
Dev Lanageuse <dev@lanageuse.com>
2025-06-13 09:26:54 +0200

    feat: implement Host component with profile display and responsive styling

--- src/components/Property/Host.jsx
+++ src/components/Property/Host.jsx
@@ -1,6 +1,20 @@
+function Host({ host }) {
+    const { picture, name } = host
+    const words = name.split(" ")
+    const firstName = words[0]
+    const lastName = words.slice(1).join(" ")
 
-function Host() {
-    return (<></>)
+    return (
+        <div className="host">
+            <div className="name">
+                <div>{firstName} </div>
+                <div>{lastName}</div>
+            </div>
+            <div className="picture">
+                <img src={picture} alt={name} />
+            </div>
+        </div>
+    )
 }
 
 export default Host;
