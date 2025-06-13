6329b276e65a5df3d75b183dd6f701cf35379008
Dev Lanageuse <dev@lanageuse.com>
2025-06-13 09:27:17 +0200

    refactor: replace img tag with SVG component in Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
 import { NavLink } from "react-router";
-import logo from '../assets/logo-footer.svg'
+import { ReactComponent as Logo } from '../assets/logo-footer.svg'
 function Footer() {
     return (
         <footer>
-            <NavLink to="/"><img src={logo} alt="Kasa" /></NavLink>
+            <NavLink to="/"><Logo /></NavLink>
             <div className='footer-container'><p>© 2020 Kasa.</p><p>All rights reserved</p></div>
         </footer>
     )
