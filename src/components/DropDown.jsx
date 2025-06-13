1963bfbb43c8d9a3819b16885433fae0f2c93aa5
Dev Lanageuse <dev@lanageuse.com>
2025-06-13 09:25:24 +0200

    feat: implement dropdown component with toggle functionality and styling

--- src/components/DropDown.jsx
+++ src/components/DropDown.jsx
@@ -1,5 +1,23 @@
-function DropDown() {
-    return (<></>)
+import { useState } from 'react';
+import { ReactComponent as Arrow } from '../assets/arrow-dropdown.svg'
+function DropDown({ title, content }) {
+    const [active, setActive] = useState(false);
+    const isArray = Array.isArray(content);
+    return (
+        <div className="dropdown-container">
+            <div className="dropdown-title" onClick={() => setActive((prev) => !prev)}>{title} <button className="btn-arrow"><Arrow className={active ? 'arrow arrow-up' : 'arrow arrow-down'} /></button></div>
+            <div className={`dropdown-content ${active ? 'active' : ''}`}>{
+                isArray ? (
+                    <ul>
+                        {content.map((item, index) => {
+                            return <li key={index}>{item}</li>
+                        })}
+                    </ul>
+                ) :
+                    (<p>{content}</p>)
+            }</div>
+        </div >
+    )
 }
 
 export default DropDown;
