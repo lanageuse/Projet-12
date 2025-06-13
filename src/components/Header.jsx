12931044fee10549fae6cee693dba189ac4fa910
Dev Lanageuse <dev@lanageuse.com>
2025-06-13 09:27:35 +0200

    refactor: replace logo img element with SVG React component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 import { NavLink } from "react-router";
-import logo from "../assets/logo.svg"
+import { ReactComponent as Logo } from "../assets/logo.svg"
 
 function Header() {
     return (
         <header>
-            <NavLink to="/"><img src={logo} alt="Kasa" className="logo" /></NavLink>
+            <NavLink to="/"><Logo /></NavLink>
             <nav>
                 <NavLink to="/">Accueil</NavLink>
                 <NavLink to="/about">A propos</NavLink>
