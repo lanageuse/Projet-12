28c17021d19561066931225385b0e2592c914ce0
Dev Lanageuse <dev@lanageuse.com>
2025-06-13 09:20:11 +0200

    feat : Add vite-plugin-svgr for SVG component support

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import sass from 'sass'
+import svgr from 'vite-plugin-svgr'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    svgr({
+      svgrOptions: { exportType: 'named', ref: true, svgo: false, titleProp: true },
+      include: '**/*.svg',
+    }),
+    react(),
+  ],
   css: {
     preprocessorOptions: {
       scss: {
