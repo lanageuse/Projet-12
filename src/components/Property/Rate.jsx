81ca62290f6a01aacab4a693c1b56ad0a91d1f5b
Dev Lanageuse <dev@lanageuse.com>
2025-06-13 09:26:14 +0200

    feat: implement star rating component with responsive design

--- src/components/Property/Rate.jsx
+++ src/components/Property/Rate.jsx
@@ -1,5 +1,17 @@
-function Rate() {
-    return (<></>)
+import { ReactComponent as StarIcon } from '../../assets/star.svg'
+
+function Rate({ rate }) {
+    return (
+        <div className='rate-container'>
+            {[...Array(5)].map((_, index) => (
+                <StarIcon
+                    key={index}
+                    fill={index < rate ? '#FF6060' : '#e3e3e3'}
+                    className='star'
+                />
+            ))}
+        </div>
+    )
 }
 
 export default Rate;
