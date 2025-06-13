77d2125c478070113e47dc73f558c3f29d6b2480
Dev Lanageuse <dev@lanageuse.com>
2025-06-13 09:25:53 +0200

    feat: implement tags component with responsive styling

--- src/components/Property/Tags.jsx
+++ src/components/Property/Tags.jsx
@@ -1,5 +1,13 @@
-function Tags() {
-    return (<></>)
+
+function Tags({ tags }) {
+
+    return (
+        <div className="tag-container">
+            {tags.map((tag, index) => (
+                <div key={index} className="tag">{tag}</div>
+            ))}
+        </div>
+    )
 }
 
 export default Tags;
