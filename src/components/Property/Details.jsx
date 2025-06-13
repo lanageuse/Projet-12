1ed333532cbeae3ea9615ff6cda349453520eb31
Dev Lanageuse <dev@lanageuse.com>
2025-06-13 09:26:35 +0200

    feat: implement property details component with description and equipment dropdowns

--- src/components/Property/Details.jsx
+++ src/components/Property/Details.jsx
@@ -1,5 +1,12 @@
-function Details() {
-    return (<></>)
+import DropDown from "../Dropdown";
+
+function Details({ description, equipments }) {
+    return (
+        <div className="details-container">
+            <DropDown title="Description" content={description} />
+            <DropDown title="Équipements" content={equipments} />
+        </div>
+    )
 }
 
 export default Details;
