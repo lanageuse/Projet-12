ace9811fda7e730eb55a4cb09236966588e2f9c6
Dev Lanageuse <dev@lanageuse.com>
2025-06-13 09:23:50 +0200

    refactor : Implement Title component with responsive styling

--- src/components/Property/Title.jsx
+++ src/components/Property/Title.jsx
@@ -1,5 +1,10 @@
-function Title() {
-    return (<></>)
+function Title({ title, location }) {
+    return (
+        <section className="title-container">
+            <h1 className="h1">{title}</h1>
+            <h2 className="h2">{location}</h2>
+        </section>
+    )
 }
 
 export default Title;
