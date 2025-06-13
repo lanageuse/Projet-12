abe244dc7b01ddd8ae5a44ffc0cbcaa0fa762397
Dev Lanageuse <dev@lanageuse.com>
2025-06-13 09:22:56 +0200

    refactor: replace SVG imports with ReactComponent and add cursor pointer to carousel arrows

--- src/components/Caroussel.jsx
+++ src/components/Caroussel.jsx
@@ -1,5 +1,5 @@
-import arrowRight from '../assets/arrow-right.svg'
-import arrowLeft from '../assets/arrow-left.svg'
+import { ReactComponent as ArrowRight } from '../assets/arrow-right.svg'
+import { ReactComponent as ArrowLeft } from '../assets/arrow-left.svg'
 import { useEffect, useState } from 'react';
 function Caroussel({ children }) {
     const [currentIndex, setCurrentIndex] = useState(0)
@@ -26,7 +26,7 @@ function Caroussel({ children }) {
         <div className="carousel-container">
             <div className="carousel-wrapper">
                 {length > 1 ? <button className="left-arrow" onClick={prev}>
-                    <img src={arrowLeft} alt="précédent" />
+                    <ArrowLeft />
                 </button> : ''}
                 <div className="carousel-content-wrapper">
                     <div className="carousel-content" style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
@@ -34,7 +34,7 @@ function Caroussel({ children }) {
                     </div>
                 </div>
                 {length > 1 ? <button className="right-arrow" onClick={next}>
-                    <img src={arrowRight} alt="suivant" />
+                    <ArrowRight />
                 </button> : ''}
 
                 {length > 1 ? <div className='current-state'>{`${currentIndex + 1} / ${length}`}</div> : ''}
