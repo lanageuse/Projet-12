7bb6451d0efb15f2b25922f0a322b2c209fa5693
Dev Lanageuse <dev@lanageuse.com>
2025-06-16 15:30:17 +0200

    feat: add error handling to listings context and component

--- src/components/Listing/Listings.jsx
+++ src/components/Listing/Listings.jsx
@@ -1,19 +1,24 @@
 import ListingsGrid from "./ListingsGrid"
 import { useListings } from '../../context/ListingsContext'
 import Listing from "./Listing"
-import { Suspense } from "react"
+import ErrorMessage from "../ErrorMessage"
+
 function Listings() {
-    const { listings, loading } = useListings()
-    if (loading) return 'Chargement....'
+    const { listings, loading, error } = useListings()
+    if (loading) return '<p>Chargement...</p>'
+
     return (
         <div className="listings">
-            <ListingsGrid fallback="Chargement...">
-                {listings.map((listing, index) => (
-                    <Listing key={index} listing={listing} />
-                )
-                )}
-            </ListingsGrid>
-        </div>
+            <ErrorMessage error={error.message} />
+            {!error &&
+                <ListingsGrid ListingsGrid fallback="Chargement...">
+                    {listings.map((listing, index) => (
+                        <Listing key={index} listing={listing} />
+                    )
+                    )}
+                </ListingsGrid>
+            }
+        </div >
     )
 }
 
--- src/context/ListingsContext.jsx
+++ src/context/ListingsContext.jsx
@@ -5,26 +5,36 @@ const ListingsContext = createContext()
 export function ListingsProvider({ children }) {
     const [listings, setListings] = useState([])
     const [loading, setLoading] = useState(true)
+    const [error, setError] = useState("");
 
     useEffect(() => {
-        fetch("../data/properties.json")
-            .then((res) => {
-                return res.json()
-            })
-            .then(res => {
-                setListings(res.listings)
-                setLoading(false)
-            })
-            .catch((error) => {
-                console.log("Erreur de chargement des données : ", error)
-                setListings([])
-                setLoading(false)
-            })
-    }, [])
+        let isMounted = true
+        const fetchData = async () => {
+            try {
+                const res = await fetch("../data/properties.json");
+                if (!res.ok) throw new error("Erreur serveur");
+                const data = await res.json()
+                if (isMounted) {
+                    setListings(data.listings)
+                    setLoading(false)
+                }
+            } catch (err) {
+                if (isMounted) {
+                    setError(err);
+                    setLoading(false);
+                }
+            }
+        }
+        fetchData()
+        return () => {
+            isMounted = false
+        }
+    }, [error])
+
 
 
     return (
-        <ListingsContext.Provider value={{ listings, loading }}>
+        <ListingsContext.Provider value={{ listings, loading, error }}>
             {children}
         </ListingsContext.Provider>
     );
