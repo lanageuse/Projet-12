a672552c9a978fcca8e699d1746523eb8250f630
Dev Lanageuse <dev@lanageuse.com>
2025-06-16 15:30:04 +0200

    feat: add ErrorMessage component with styling

---
+++ src/components/ErrorMessage.jsx
@@ -0,0 +1,11 @@
+function ErrorMessage({ error }) {
+    if (!error) return null;
+    return (
+        <div className="error-container">
+            <p>Une erreur est survenue :</p>
+            <pre>{error}</pre>
+        </div>
+    )
+}
+
+export default ErrorMessage;
