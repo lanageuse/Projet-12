2f60809dc7907984c7d8f0f39cedaef097396553
Dev Lanageuse <dev@lanageuse.com>
2025-06-16 09:45:51 +0200

    refactor: redesign 404 NotFound page with improved styling and navigation

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,11 @@
-function NotFound(){
-    return(
-        <div>
-            <h3>404 NOT FOUND</h3>
+import { NavLink } from "react-router"
+
+function NotFound() {
+    return (
+        <div className="notFound-container">
+            <div className="title">404</div>
+            <div className="content">Oups! La page que vous demandez n'existe pas.</div>
+            <div className="link"><NavLink to="/">Retourner sur la page d’accueil</NavLink></div>
         </div>
     )
 }
