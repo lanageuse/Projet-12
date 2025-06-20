0bd1133375994dcbd345fb88a0429112bec08ce6
Dev Lanageuse <dev@lanageuse.com>
2025-06-20 10:41:07 +0200

    feat: add useFetchData custom hook for property listings

---
+++ src/hook/useFetchData.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useReducer } from "react";
+import listingsReducer from '../hook/reducers/listingsReducer'
+function useFetchData() {
+    const [state, dispatch] = useReducer(listingsReducer, {
+        listings: null,
+        error: null,
+        status: "fetching",
+    })
+
+    useEffect(() => {
+        const fetchData = async () => {
+            dispatch({ type: "fecthing" })
+            try {
+                const res = await fetch("../data/properties.json");
+                if (!res.ok) throw new Error("Erreur serveur");
+                const data = await res.json()
+                dispatch({
+                    type: "done",
+                    payload: data.listings
+                })
+            } catch (err) {
+                dispatch({
+                    type: "fail",
+                    error: err
+                })
+            }
+        }
+        fetchData()
+    }, [])
+    return state
+}
+
+export default useFetchData
