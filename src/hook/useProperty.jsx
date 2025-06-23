a0e3f8a17c5a56ed0697368687ef7c4eda837a32
Dev Lanageuse <dev@lanageuse.com>
2025-06-23 10:47:12 +0200

    Remove debug console.log from useProperty hook

--- src/hook/useProperty.jsx
+++ src/hook/useProperty.jsx
@@ -3,7 +3,6 @@ import { useListings } from "../context/ListingsContext"
 function useProperty(id) {
     const { listings, status } = useListings()
     const property = listings.find(prop => prop.id === id) || null;
-    console.log(property, status);
     return { property, status };
 }
 
