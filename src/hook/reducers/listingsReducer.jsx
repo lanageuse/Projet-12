e6b729d58c56063f97455e2df5d481e47e9f1e9d
Dev Lanageuse <dev@lanageuse.com>
2025-06-23 10:47:48 +0200

    Fix typo in action type and improve error logging in listingsReducer

--- src/hook/reducers/listingsReducer.jsx
+++ src/hook/reducers/listingsReducer.jsx
@@ -1,14 +1,15 @@
 
 function listingsReducer(state, action) {
+    console.log(action.type);
     switch (action.type) {
-        case "fecthing":
+        case "fetching":
             return { status: "fetching", listings: null, error: null }
         case "done":
             return { status: "done", listings: action.payload, error: null }
         case "fail":
             return { status: "fail", listings: null, error: action.error }
         default:
-            throw new Error("action inconnu !")
+            throw new Error("action inconnu ! : ", action.type)
     }
 }
 
