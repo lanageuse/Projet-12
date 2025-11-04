f9da7842c420ccbd78fe0cefddf14e36e18f363f
Dev Lanageuse <dev@lanageuse.com>
2025-11-04 10:46:29 +0100

    Refactor components: fix import path and apply consistent code formatting

--- src/components/About/DropDownList.jsx
+++ src/components/About/DropDownList.jsx
@@ -1,20 +1,20 @@
-import DropDown from "../DropDown"
-import ErrorMessage from "../ErrorMessage";
-import Loading from "../Loading";
+import DropDown from '../Dropdown';
+import ErrorMessage from '../ErrorMessage';
+import Loading from '../Loading';
 
 function DropDownList({ dropDownList }) {
-    const { data, status, error } = dropDownList;
+  const { data, status, error } = dropDownList;
 
-    if (status === "fetching") return <Loading />;
-    if (status === "fail") return <ErrorMessage error={error} />;
+  if (status === 'fetching') return <Loading />;
+  if (status === 'fail') return <ErrorMessage error={error} />;
 
-    return (
-        <>
-            {data.map((drop, index) => (
-                <DropDown key={index} title={drop.title} content={drop.content} />
-            ))}
-        </>
-    );
+  return (
+    <>
+      {data.map((drop, index) => (
+        <DropDown key={index} title={drop.title} content={drop.content} />
+      ))}
+    </>
+  );
 }
 
 export default DropDownList;
--- src/components/DropDown.jsx
+++ src/components/DropDown.jsx
@@ -1,23 +1,30 @@
 import { useState } from 'react';
-import { ReactComponent as Arrow } from '../assets/arrow-dropdown.svg'
+import { ReactComponent as Arrow } from '../assets/arrow-dropdown.svg';
+
 function DropDown({ title, content }) {
-    const [active, setActive] = useState(false);
-    const isArray = Array.isArray(content);
-    return (
-        <div className="dropdown-container">
-            <div className="dropdown-title" onClick={() => setActive((prev) => !prev)}>{title} <button className="btn-arrow"><Arrow className={active ? 'arrow arrow-up' : 'arrow arrow-down'} /></button></div>
-            <div className={`dropdown-content ${active ? 'active' : ''}`}>{
-                isArray ? (
-                    <ul>
-                        {content.map((item, index) => {
-                            return <li key={index}>{item}</li>
-                        })}
-                    </ul>
-                ) :
-                    (<p>{content}</p>)
-            }</div>
-        </div >
-    )
+  const [active, setActive] = useState(false);
+  const isArray = Array.isArray(content);
+  return (
+    <div className="dropdown-container">
+      <div className="dropdown-title" onClick={() => setActive((prev) => !prev)}>
+        {title}{' '}
+        <button className="btn-arrow">
+          <Arrow className={active ? 'arrow arrow-up' : 'arrow arrow-down'} />
+        </button>
+      </div>
+      <div className={`dropdown-content ${active ? 'active' : ''}`}>
+        {isArray ? (
+          <ul>
+            {content.map((item, index) => {
+              return <li key={index}>{item}</li>;
+            })}
+          </ul>
+        ) : (
+          <p>{content}</p>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default DropDown;
